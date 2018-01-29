4a8f66814a0253e32dbfa97e4d117463d7f98324
SEAN OLEJAR <solejar236@gmail.com>
2018-01-29 12:37:50 -0500

    just committing before leaving

---
+++ stringCount.c
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<stdio.h>
+using namespace std;
+
+int main(int argc, char ** argv){
+
+	string fileName = argv[0]
+	
+	int stringCount = argc-2;
+	
+	char 
+	for int(i = 2;i<argc;i++){
+	
+	}
+	return 0;
+}
