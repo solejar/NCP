bfa615d5634cdd0e30d971ace8b6706d15e61670
SEAN OLEJAR <solejar236@gmail.com>
2018-02-05 11:21:03 -0500

    basic structure done

--- proj0-5/stringCount.c
+++ proj0-5/stringCount.c
@@ -1,28 +1,34 @@
 #include<stdio.h>
 #include<string.h>
 
-struct substrLink
+typedef struct node
 {
 	char val;
+	struct node * next;
 	 	
-};
+} substrNode;
 
 int main(int argc, char ** argv){
 	//let's read in input substrings, and put them into a search tree of sorts
-        struct substrLink inputString[26];
+        substrNode inputString[26];
+
+	for(int i = 0;i<26;i++){
+		inputString[i].val = 97+i; //this range represents all 26 lowercase letters 
+                //printf("Just added %c\n",inputString[i].val);
+	}
 
         int sysCallImplementation,lastString;
         if(strcmp(argv[argc-1],"--systemcalls")==0){
                 sysCallImplementation = 1;
                 lastString = argc-2;
-		printf("Supposedly, %s = --systemcalls",argv[argc-1]);
+		printf("Supposedly, %s = --systemcalls\n",argv[argc-1]);
         }else{
                 sysCallImplementation = 0;
                 lastString = argc-1;
-		printf("Supposedly, %s != --systemcalls",argv[argc-1]);
+		printf("Supposedly, %s != --systemcalls\n",argv[argc-1]);
         }
         
-        for (int i = 2;i<=lastString;i++){
+        for (int i = 1;i<=lastString;i++){
                 char * temp = argv[i];
     		printf("read in substring of: %s \n",temp);
 	}	 
