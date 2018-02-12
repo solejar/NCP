53149616a294467af70a330e5f45197d4868a118
SEAN OLEJAR <solejar236@gmail.com>
2018-02-12 12:14:08 -0500

    this is basically pointless lol this needs to work for any substring, not just english sentece.

--- proj0-5/stringCount.c
+++ proj0-5/stringCount.c
@@ -36,6 +36,47 @@ typedef struct node
 
 } substrNode;
 
+typedef struct node2
+{
+	char val;
+	struct node[26] childArr;
+} substrNode2;
+
+substrNode2* substrNode2Creator(char nodeVal){
+
+	substrNode2* newNode = malloc(sizeof(substrNode));
+	newNode->val = nodeVal;
+	
+	substrNode2* arrPtr = malloc(26*sizeof(substrNode));
+
+	newNode->childArr = arrPtr;
+
+	return newNode;
+}
+
+void populateNode2(substrNode2* source, char newChar, substrNode2 nodeVal){
+	int charInt = (int) newChar;
+	int index = charInt-97;
+	if(index>0&&index<26){
+		substrNode2[] temp = source->childArr;
+		temp[index]=nodeVal;
+		source->childArr = temp;
+	}else{
+		printf("woops, you have some weird substring passed in");
+	}
+}
+
+substrNode2* getNode2(substrNode2* source, char getChar){
+
+	int charInt = (int) getChar;
+	int index = charInt - 97;
+	if(index>0&&index<26){
+		
+		return (source->childArr +index);
+	}else{
+		printf("woops, you have some weird char passed in");
+	}
+}
 /*
 typedef struct listElement
 {
@@ -72,6 +113,8 @@ substrNode* substrNodeCreator(char nodeVal){
 	newNode->x = malloc(sizeof(substrNode));
 	newNode->y = malloc(sizeof(substrNode));
 	newNode->z = malloc(sizeof(substrNode));
+
+	return newNode;
 };
 
 void substrNodeDestroyer(substrNode*){
@@ -141,12 +184,114 @@ void populateNode (substrNode* source, char newChar){
 			source->t = substrNodeCreator('t');
 			break;
 		case 'u':
-			source->a = substrNodeCreator('a');
+			source->u = substrNodeCreator('u');
+			break;
+		case 'v':
+			source->v = substrNodeCreator('v');
+			break;
+		case 'w':
+			source->w = substrNodeCreator('w');
+			break;
+		case 'x':
+			source->x = substrNodeCreator('x');
+			break;
+		case 'y':
+			source->y = substrNodeCreator('y');
+			break;
+		case 'z':
+			source->z = substrNodeCreator('z');
 			break;
 		default:
+			break;
 
 	}
 }; 
+
+substrNode* getNode(substrNode* source, char newChar){
+	switch(newChar){
+		case 'a':
+			return source->a;
+			break;
+		case 'b':
+			return source->b;
+			break;
+		case 'c':
+			return source->c;
+			break;
+		case 'd':
+			return source->d;
+			break;
+		case 'e':
+			return source->e;
+			break;
+		case 'f':
+			return source->f;
+			break;
+		case 'g':
+			return source->g;
+			break;
+		case 'h':
+			return source->h;
+			break;
+		case 'i':
+			return source->i;
+			break;
+		case 'j':
+			return source->j;
+			break;
+		case 'k':
+			return source->k;
+			break;
+		case 'l':
+			return source->l;
+			break;
+		case 'm':
+			return source->m;
+			break;
+		case 'n':
+			return source->n;
+			break;
+		case 'o':
+			return source->o;
+			break;
+		case 'p':
+			return source->p;
+			break;
+		case 'q':
+			return source->q;
+			break;
+		case 'r':
+			return source->r;
+			break;
+		case 's':
+			return source->s;
+			break;
+		case 't':
+			return source->t;
+			break;
+		case 'u':
+			return source->u;
+			break;
+		case 'v':
+			return source->v;
+			break;
+		case 'w':
+			return source->w;
+			break;
+		case 'x':
+			return source->x;
+			break;
+		case 'y':
+			return source->y;
+			break;
+		case 'z':
+			return source->z;
+			break;
+		default:
+			return source->a;
+
+	}
+};
 
 int main(int argc, char ** argv){
 	//let's read in input substrings, and put them into a search tree of sorts
@@ -154,7 +299,7 @@ int main(int argc, char ** argv){
 
 	for(int i = 0;i<26;i++){
 		char newChar = (char) (97 + i);
-		inputString[i] = substrNodeCreator(newChar); //this range represents all 26 lowercase letters 
+		inputString[i] = *substrNode2Creator(newChar); //this range represents all 26 lowercase letters 
                 //printf("Just added %c\n",inputString[i].val);
 	}
 
@@ -172,6 +317,21 @@ int main(int argc, char ** argv){
         for (int i = 2;i<=lastString;i++){
                 char * temp = argv[i];
     		printf("read in substring of: %s \n",temp);
+		
+		int curr =(int) temp[0];
+		substrNode2 headNode =  inputString[curr-97];
+	
+		substrNode2* currNodePtr = *headNode;
+		
+		for(int i = 1;i<strlen(temp);i++){
+			int curr = (int) temp[i];
+			substrNode2* childNodePtr = getNode2(currNodePtr,temp[i]);
+			if(!childNodePtr){
+				childNodePtr = substrNode2Creator(temp[i]);
+			}
+
+			currNodePtr = childNodePtr;				
+		}
 	}	 
 	//let's open up the file and read in characters (need to decide what implementation is best)
 	char *fileName = argv[1];
